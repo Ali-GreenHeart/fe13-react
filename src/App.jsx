40ed0bd314f87c9f2e3ff702b216b5b41531e3c8
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-07-03 21:25:35 +0400

    add Counter component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
-import Button from "./components/Button"
-import Header from "./components/Header"
-import Home from "./Home"
+import Counter from "./components/Counter";
 
 function App() {
     return (
         <>
-            <Header title="welcome react!" />
-            <Button
-                title="click me"
-                text="salam ay react!"
-            />
-            <Button
-                title="click me2"
-                text="react-a xos gelmisiniz!"
-            />
-
-            <Home />
+            <Counter />
         </>
     )
 }

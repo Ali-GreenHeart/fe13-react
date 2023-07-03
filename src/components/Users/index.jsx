966613f7770b38a76a07b0ad72165084ea862280
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-07-03 22:32:05 +0400

    add Users

--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -1,30 +1,51 @@
 import axios from "axios"
 import { useState } from "react"
+import "./index.css"
 
 const url = `https://jsonplaceholder.typicode.com/users`
 let componentYarandimi = false
 const Users = () => {
     const [users, setUsers] = useState([])
+    const [isDark, setIsDark] = useState(false)
+
 
     if (componentYarandimi === false) {
         axios.get(url).then(({ data }) => {
             setUsers(data)
             componentYarandimi = true
+            let fromStorage = localStorage.getItem('isDark')
+            setIsDark(fromStorage === "true" ? true : false)
         })
     }
 
 
+
     return (
-        <>
+        <div
+            className={isDark ? "darkContainer" : null}
+        >
+            <button
+                onClick={() => {
+                    let newIsDark = !isDark
+                    setIsDark(newIsDark)
+                    localStorage.setItem('isDark', newIsDark)
+                }}
+            >{isDark ? '🌞' : '🌝'}</button>
             {
                 users.map((user) => {
                     return <div key={user.id}>
-                        <h1>{user.username}</h1>
-                        <p>{user.name}</p>
+                        <h1>
+                            <button
+                                onClick={() => {
+                                    setUsers(users.filter((deletedUser) => deletedUser.id !== user.id))
+                                }}
+                            >x</button>
+                            {user.username}
+                        </h1>
                     </div>
                 })
             }
-        </>
+        </div>
     )
 }
 export default Users

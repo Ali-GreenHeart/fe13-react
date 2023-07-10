d8b6497b7ef3c3690bd339bf1bda44da3d7aeb10
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-07-10 21:32:32 +0400

    first react-router codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Route, Routes } from "react-router";
 import Calculator from "./components/Calculator";
 
 function App() {
     return (
         <>
-            <Calculator />
+            <Routes>
+                <Route path="/" element={<div>this is ana sehife</div>} />
+                <Route path="/about" element={<p>about</p>} />
+                <Route path="/calculator" element={<Calculator />} />
+            </Routes>
         </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { BrowserRouter } from "react-router-dom";
 
 const rootElem = document.getElementById('root')
 
-ReactDOM.createRoot(rootElem).render(<App />)
+ReactDOM.createRoot(rootElem).render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+)

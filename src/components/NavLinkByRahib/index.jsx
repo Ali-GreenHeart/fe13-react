77c0ed8b082015ef5473fe42d2b3099c57b8521e
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-07-10 22:29:50 +0400

    add module.css

--- src/components/NavLinkByRahib/index.jsx
+++ src/components/NavLinkByRahib/index.jsx
@@ -1,17 +1,17 @@
 import { NavLink } from "react-router-dom"
+import styles from "./index.module.css"
+
+const style = {
+    color: 'red',
+    backgroundColor: 'black'
+}
 
 const NavLinkByRahib = ({ title, to }) => {
     return (
         <>
             <NavLink
-                style={({ isActive }) => {
-                    if (isActive) {
-                        return {
-                            color: 'red',
-                            backgroundColor: 'black'
-                        }
-                    }
-                }}
+                // style={({ isActive }) => isActive ? style : null}
+                className={({ isActive }) => isActive ? styles.link : null}
                 to={to}>
                 {title}
             </NavLink>

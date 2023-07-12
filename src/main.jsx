f15fee45475447c65711c8c6f386dd0a46d18bda
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-07-12 21:37:01 +0400

    add FullNameContext

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from "react-router-dom";
+import FullNameContext from './context/FullNameContext';
 
 const rootElem = document.getElementById('root')
 
 ReactDOM.createRoot(rootElem).render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <FullNameContext>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </FullNameContext>
 )

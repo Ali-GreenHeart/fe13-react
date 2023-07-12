379157fd171b47715a727097072ea73c81143035
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-07-12 22:11:22 +0400

    use ModeContext, initial form

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,11 +2,20 @@ import { useContext } from "react";
 import { FN_Context } from "../../context/FullNameContext";
 import NavLinkByRahib from "../NavLinkByRahib";
 import "./header.css";
+import { DarkModeContext } from "../../context/DarkModeContext";
 
 function Header() {
     const { ad } = useContext(FN_Context)
+    const [mode, setMode] = useContext(DarkModeContext)
     return <nav>
-        <p>Xos gelifsen, {ad}</p>
+        <p style={{ margin: 0 }}>Xos gelifsen, {ad}</p>
+        <button
+            onClick={() => {
+                let newMode = (mode === 'dark' ? 'light' : 'dark')
+                setMode(newMode)
+                localStorage.setItem('mode', newMode)
+            }}
+        >{mode === 'dark' ? '🌞' : '🌝'}</button>
         <NavLinkByRahib title="Home" to="/" />
         <NavLinkByRahib title="About" to="/about" />
         <NavLinkByRahib title="calculator" to="/calculator" />
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from "react-router-dom";
 import FullNameContext from './context/FullNameContext';
+import DarkModeContextComponent from './context/DarkModeContext';
 
 const rootElem = document.getElementById('root')
 
 ReactDOM.createRoot(rootElem).render(
-    <FullNameContext>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </FullNameContext>
+    <DarkModeContextComponent>
+        <FullNameContext>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </FullNameContext>
+    </DarkModeContextComponent>
 )

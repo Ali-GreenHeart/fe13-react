836ea810c29512839961c046243a9d3485bd7355
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-07-19 21:56:03 +0400

    add Rahib (actions obj)

--- src/pages/CounterWithRedux/index.jsx
+++ src/pages/CounterWithRedux/index.jsx
@@ -1,12 +1,13 @@
 import { connect } from "react-redux"
-
+import { actions } from "../../reducers/rootReducer";
 const CounterWithRedux = ({ dispatch, counter }) => {
     return (
         <>
             <h1>{counter}</h1>
-            <button onClick={() => dispatch({ type: 'inc' })}>+</button>
-            <button onClick={() => dispatch({ type: 'dec' })}>-</button>
-            <button onClick={() => dispatch({ type: 'reset' })}>0</button>
+            <button onClick={() => dispatch({ type: actions.inc })}>+</button>
+            <button onClick={() => dispatch({ type: actions.dec })}>-</button>
+            <button onClick={() => dispatch({ type: actions.reset })}>0</button>
+            <button onClick={() => dispatch({ type: actions.special, payload: +prompt("enter your favorite number") })}>RAHIB  </button>
         </>
     )
 }

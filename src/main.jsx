57fd31424f4f330056284247d09a6cc68f7a0d03
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-07-19 21:42:54 +0400

    create store and provide it

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,22 @@ import App from './App'
 import { BrowserRouter } from "react-router-dom";
 import FullNameContext from './context/FullNameContext';
 import DarkModeContextComponent from './context/DarkModeContext';
+import { createStore } from 'redux';
+import rootReducer from './reducers/rootReducer';
+import { Provider } from 'react-redux';
 
 const rootElem = document.getElementById('root')
 
+const store = createStore(rootReducer)
+
 ReactDOM.createRoot(rootElem).render(
-    <DarkModeContextComponent>
-        <FullNameContext>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </FullNameContext>
-    </DarkModeContextComponent>
+    <Provider store={store}>
+        <DarkModeContextComponent>
+            <FullNameContext>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </FullNameContext>
+        </DarkModeContextComponent>
+    </Provider>
 )
